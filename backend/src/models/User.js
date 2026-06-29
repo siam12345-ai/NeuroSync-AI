1944d2627b73e1be8d423ffd610e93978edaf0fe
siam12345-ai <abubakarsiamstd@gmail.com>
2026-06-29 21:22:43 +0600

    Added user model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,39 @@
+const mongoose = require("mongoose");
 
+const userSchema = new mongoose.Schema({
+
+name:{
+type:String,
+required:true,
+trim:true
+},
+
+email:{
+type:String,
+required:true,
+unique:true,
+trim:true,
+lowercase:true
+},
+
+password:{
+type:String,
+required:true,
+minlength:6
+},
+
+role:{
+type:String,
+enum:["user","teacher","admin"],
+default:"user"
+}
+
+},
+{
+timestamps:true
+});
+
+module.exports = mongoose.model(
+"User",
+userSchema
+);
