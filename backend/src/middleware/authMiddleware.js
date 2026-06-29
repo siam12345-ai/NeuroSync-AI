2aed9110961ce63e3c49990f57498e65ce751470
siam12345-ai <abubakarsiamstd@gmail.com>
2026-06-29 21:21:27 +0600

    Added authentication middleware

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,1 +1,52 @@
+const jwt = require("jsonwebtoken");
+const {
+    errorResponse
+} = require("../utils/response");
 
+const verifyToken = (req, res, next) => {
+
+    try {
+
+        // Authorization Header
+        const authHeader = req.headers.authorization;
+
+        // Token Check
+        if (!authHeader) {
+
+           return errorResponse(
+              res,
+              401,
+             "Access Denied. No Token Provided."
+        );
+
+   }
+
+        // Bearer Token
+        const token = authHeader.split(" ")[1];
+
+        // Verify Token
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        // Save User Info
+        req.user = decoded;
+
+        next();
+
+    }
+
+    catch (error) {
+
+        return errorResponse(
+          res,
+          401,
+          "Invalid or Expired Token"
+        );
+
+    }
+
+};
+
+module.exports = verifyToken;
